4fb3762ecc48db7313c63ef9582d90a033e5df5b
Tim Miller <innerlogic4321@gmail.com>
2022-01-16 14:12:26 -0500

    Add Test Token

---
+++ DrasticBomb.Tests/TestApiTokens.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="TestApiTokens.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace DrasticBomb.Tests
+{
+    /// <summary>
+    /// Test API Tokens.
+    /// </summary>
+    public static class TestApiTokens
+    {
+        /// <summary>
+        /// Gets the test API token.
+        /// </summary>
+        public static string API => "{{ Replace me! }}";
+    }
+}
